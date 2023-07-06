b72f604c22c2d08b5fe5aeab6c24d7cae8069097
Mauro <mauromaio.dev@gmail.com>
2023-07-06 16:22:43 -0300

    update in name box

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,10 +107,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Mauro Maio
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Front End Developer
                 </Typography>
               </Box>
             </Box>
